e66c6b41d285a00c06ad20d6e72e4e0998ed2638
karthik svs <svskarthik98@gmail.com>
2019-09-23 17:35:45 +0530

    my Module 1

---
+++ src/Module1_AOC/AreaOfCircle.java
@@ -0,0 +1,38 @@
+package Module1_AOC;
+
+/**
+ * This class is to find the area and perimeter of a cirlce.
+ * 
+ * @author Siva Sankar
+ */
+public class AreaOfCircle {
+    
+    private static final double PI = 3.14159;
+
+    /**
+     * This method calculates the area of the circle.
+     * Use the PI variable defined in the line number 8.
+     * 
+     * @param radius, the radius of the circle
+     * @return the area of the circle
+     */
+    public static double areaOfCircle(double radius) {
+        //  Your code goes here....
+        double pre=(PI*radius)*radius;
+        return pre;
+    }
+
+    /**
+     * This method returns the perimiter of the circle.
+     * Use the PI variable defined in the line number 8.
+     * 
+     * @param radius, the radius of the circle.
+     * @return the perimeter of the circle.
+     */
+    public static double perimeterOfCircle(double radius) {
+        //  Your code goes here....
+        double are=2*PI*radius;
+        return are;
+    }
+
+}
