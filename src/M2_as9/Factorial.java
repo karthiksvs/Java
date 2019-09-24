cd8ab6716907162b0697dff7d5ab65666a602be7
karthik svs <svskarthik98@gmail.com>
2019-09-24 10:17:18 +0530

    my Module 1

---
+++ src/M2_as9/Factorial.java
@@ -0,0 +1,31 @@
+package M2_as9;
+
+/**
+ * 
+ */
+
+ public class Factorial {
+
+    /**
+     * This method finds the factorial of a value n. If n is in the range of 
+     * 1 to 20 (inclusive) and retuns the factorial otherwise -1
+     * @param n, the integer n.
+     * @return factorial of n if n is in 1 to 20 (inclusive) and -1 otherwise.
+     */
+    public static long factorial(int n)
+    {
+        long fact=1;
+        if(n>0 && n<21) {
+            for (int i = 1; i <n+1; i++) {
+                    fact=fact*i;
+            }
+            return fact;
+        }
+        else if(n==0){
+            return 1;
+        }
+        else
+            return -1;
+    }
+
+ }
